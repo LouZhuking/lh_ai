3579af8063ec11db1c397b7534bbda1b545611d5
LouZhuking <1612972407@qq.com>
2025-07-21 21:37:57 +0800

    '接口联调'

---
+++ react/api_demos/jwt-api-demo/src/App.jsx
@@ -0,0 +1,49 @@
+import {
+  useState,
+  useEffect
+} from 'react'
+import './App.css'
+import {
+  getTodos,
+  getRepos
+} from '@/api'
+
+function App() {
+  // const [todos, setTodos] = useState([])
+  const [repos, setRepos] = useState([])
+  useEffect(() => {
+    const fetchDate = async () => {
+      // const todosResult = await getTodos()
+      // console.log(todosResult);
+      // setTodos(todosResult.data.data)
+      const reposResult = await getRepos()
+      // console.log(reposResult);
+      setRepos(reposResult.data)
+    }
+    fetchDate()
+  }, [])
+
+  return (
+    <>
+      {/* <h1>todos</h1>
+      {
+        todos.map(todo => (
+          <div key={todo.id}>
+            {todo.title}
+          </div>)
+        )
+      } */}
+      <h1>repos</h1>
+      {
+        repos.map(repo => (
+          <div key={repo.id}>
+            {repo.title}
+            {repo.description}
+          </div>)
+        )
+      }
+    </>
+  )
+}
+
+export default App
