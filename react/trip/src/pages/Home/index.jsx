48c864283715cc00088d7fee58e93f464ee8fbed
LouZhuking <1612972407@qq.com>
2025-07-24 21:29:36 +0800

    '路由配置和tabbar组件'

---
+++ react/trip/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import useTitle from "@/hooks/useTitle"
+
+const Home = () => {
+  useTitle("Happy Trip Home")
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+export default Home
---
+++ react/trip/src/pages/Search/index.jsx
@@ -0,0 +1,10 @@
+import useTitle from "@/hooks/useTitle"
+const Search = () => {
+  useTitle("Search")
+  return (
+    <div>
+      <h1>Search</h1>
+    </div>
+  )
+}
+export default Search
