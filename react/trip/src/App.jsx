71990c3557c63271149f19a850bfa1758925a147
LouZhuking <1612972407@qq.com>
2025-07-24 16:57:54 +0800

    '移动端适配等初始项目'

---
+++ react/trip/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div
+        style={{
+          width: '3.46667rem',
+          height: '5rem',
+          background: 'yellow',
+        }}></div>
+      <div className="box"></div>
+    </>
+  )
+}
+
+export default App
