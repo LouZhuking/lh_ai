fba38ca6f79416658045e21eeddf649848f567a9
LouZhuking <1612972407@qq.com>
2025-09-11 21:36:17 +0800

    'phone-gap-home'

---
+++ rag/phonegpt/phone-gap/components/ChatInput.tsx
@@ -0,0 +1,35 @@
+"use client"
+interface ChatInputProps {
+  input: string;
+  handleInputChange: (e: any) => void;
+  handleSubmit: (e: any) => void;
+}
+import {
+  Input
+} from '@/components/ui/input'
+import {
+  Button
+} from '@/components/ui/button'
+import {
+  ArrowUp
+} from 'lucide-react'
+// const ChatInput: React.FC<ChatInputProps> = ({})
+export default function ChatInput({
+  input,
+  handleInputChange,
+  handleSubmit
+}: ChatInputProps) {
+  return (
+    <form onSubmit={handleSubmit} className="flex gap-2">
+      <Input
+        onChange={handleInputChange}
+        value={input}
+        placeholder="Ask me something about phone..."
+      />
+      <Button>
+        <ArrowUp />
+        <span className='sr-only'>Submit</span>
+      </Button>
+    </form>
+  )
+}
